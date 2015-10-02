308a6cb22ff3934bdafcfd105a9d2bf42f7fe2b3
Edouard Boissel <creasitenet.com@gmail.com>
2015-10-02 10:33:54 +0200

    delete unused file

--- fuel/app/views/galerie/ajouter.php
+++
@@ -1,7 +0,0 @@
-<h3>Ajouter un Album
-	<span class="pull-right">
-	<?php echo Html::anchor('albums', '<i class="fa fa-chevron-left"></i> RETOUR', array('class' => 'btn btn-default btn-sm')); ?>
-	</span>
-</h3>
-
-<?php echo render('albums/_form'); ?>
